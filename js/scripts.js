0683c5a96699473e33bfc1c016872d7e337559a1
Melissa <melissa.scmi@gmail.com>
2021-08-25 10:02:17 -0700

    pass test 1: function to roll random # between 1 & 6
    Co-authored-by: Art Weinstein <artur.weinstein@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+
+
+// die roll function
+let min = 1;
+let max = 6;
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
